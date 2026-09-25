ce64db2364abbe9b11d51a1a65759e181cbfd8c9
ezrakayeyu3000-debug <ezrakayeyu3000@gmail.com>
2026-09-25 14:08:38 +0800

    Add Task1 class to print greeting message

---
+++ Tasks/Task1.java
@@ -0,0 +1,9 @@
+/**
+ * The Task1 class implements an application that
+ * simply prints "Kamusta Mundo!" to standard output.
+ */
+public class Task1 {
+    public static void main(String[] args) {
+        System.out.println("Kamusta Mundo!"); // Display the string.
+    }
+}
